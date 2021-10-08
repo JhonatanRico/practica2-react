ec9f4173c339e863f5f18ecba9f18855a8b70d36
JhonatanRico <90428854+JhonatanRico@users.noreply.github.com>
2021-10-07 17:36:13 -0700

    Se modifico el proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Banner from './Componentes/Banner';
+import Body from './Componentes/Body';
+import Header from './Componentes/Header';
+import Footer from './Componentes/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Banner tituloban1= "Desarrollo de Aplicaciones de Dispositivos Moviles"/>
+     <Header
+     
+     /> 
+     <Banner tituloban2= "8/Oct/2021"/>
+     <Body
+      practica="Practica 1-Unidad 2"  
+       tema1 = "1.-Tipos de Componentes"
+       tema2 = "2.-Contenedores"
+       tema3 = "3.-Usar mas de un componente"
+       tema4 = "4.-Funciones"
+       tema5 = "5.-Props"
+       tema6 = "6.-Prop-types"
+       tema7 = "7.-DefaultProps"
+     />  
+     <Footer
+     pie= "Copyright ©; Todos los derechos reservados" 
+     />
     </div>
+     
+      
   );
 }
 
